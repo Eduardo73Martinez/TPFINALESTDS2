a9c5f781bd7f60ea896611f66d0d9b07053fe14a
Eduardo <121836028+Eduardo73Martinez@users.noreply.github.com>
2023-11-22 16:43:33 -0300

    Add files via upload

---
+++ Equipo.h
@@ -0,0 +1,59 @@
+#ifndef _EQUIPO_H
+#define _EQUIPO_H
+
+#include <iostream>
+
+#define CANTIDAD_SELECCIONES 16
+
+using namespace std;
+
+struct EquipoSt;
+typedef EquipoSt* Equipo;
+
+enum Seleccion {
+    ESTADOSUNIDOS,
+    COSTARICA,
+    COLOMBIA,
+    PARAGUAY,
+    HAITI,
+    PERU,
+    BRASIL,
+    ECUADOR,
+    VENEZUELA,
+    MEXICO,
+    URUGUAY,
+    JAMAICA,
+    ARGENTINA,
+    BOLIVIA,
+    CHILE,
+    PANAMA,
+    //europeas las agrego en  caso de querer jugar otra competencia tambien.
+    FRANCIA,
+    ESPAÑA,
+    SUIZA,
+    BELGICA,
+    CROACIA,
+    PORTUGAL,
+    ITALIA, 
+    ALEMANIA,
+    AUSTRIA,
+    ESLOVENIA,
+    NORUEGA,
+    SUECIA, 
+    ESTONIA,
+    LETONIA,
+    POLONIA,
+    HOLANDA
+};
+
+Equipo crearEquipo(Seleccion seleccion, string grupo);
+
+string nombreEquipo(Equipo e);
+
+string grupoEquipo(Equipo e);
+
+Seleccion seleccionEquipo(Equipo e);
+
+string nombreDeSeleccion(Seleccion s);
+
+#endif
